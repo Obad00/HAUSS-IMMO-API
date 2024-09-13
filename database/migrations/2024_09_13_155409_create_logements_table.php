e60e48987bd735c820ed10cda723332b261dff53
Adama Dabo <dabodabo9064@gmail.com>
2024-09-13 16:46:20 +0000

    J'ai eu à configurer mon fichier .env et après créer les migrations avec les models correspondants

---
+++ database/migrations/2024_09_13_155409_create_logements_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('logements', function (Blueprint $table) {
+            $table->id();
+            $table->string('titre');
+            $table->string('adresse');
+            $table->enum('type', ['appartement', 'maison']);
+            $table->string('image')->nullable();
+            $table->string('ville');
+            $table->string('region');
+            $table->string('quartier');
+            $table->integer('nombreChambre');
+            $table->integer('nombreToilette');
+            $table->integer('nombreEtage')->nullable();
+            $table->integer('surface');
+            $table->text('description');
+            $table->decimal('prix', 10, 2);
+            $table->foreignId('proprietaire_id')->constrained('proprietaires')->onDelete('cascade');
+            $table->foreignId('categorie_id')->nullable()->constrained('categories')->onDelete('set null');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('logements');
+    }
+};
---
+++ database/migrations/2024_09_13_155432_create_reservations_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->enum('statut', ['en attente', 'acceptee', 'declinee']);
+            $table->foreignId('logement_id')->constrained('logements')->onDelete('cascade');
+            $table->foreignId('locataire_id')->constrained('locataires')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservations');
+    }
+};
